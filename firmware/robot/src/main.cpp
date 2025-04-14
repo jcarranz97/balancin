29af5e7e020e74c75ee427fd1098f945e7e899a9
Juan Carranza <juancarranza7561@gmail.com>
2025-04-13 22:49:39 -0500

    Include MPU6050 (#3)
    * Simplify FREE RTOS task
    
    * Remove async worker
    
    * Include init and method to determine if the MPu is found
    
    * Fix pre-commit hoooks
    
    * Include link to register map
    
    * Use library from i2cdevlib (#2)

--- firmware/robot/src/main.cpp
+++ firmware/robot/src/main.cpp
@@ -11,135 +11,56 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "mpu6050.h"
-
-// Which core to run on if configNUMBER_OF_CORES==1
-#ifndef RUN_FREE_RTOS_ON_CORE
-#define RUN_FREE_RTOS_ON_CORE 0
-#endif
-
-// Whether to flash the led
-#ifndef USE_LED
-#define USE_LED 1
-#endif
-
-// Whether to busy wait in the led thread
-#ifndef LED_BUSY_WAIT
-#define LED_BUSY_WAIT 1
-#endif
-
-// Delay between led blinking
-#define LED_DELAY_MS 500
+#include "hardware/i2c.h"
+#include "MPU6050.h"
 
 // Priorities of our threads - higher numbers are higher priority
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 2UL )
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 1UL )
-#define WORKER_TASK_PRIORITY    ( tskIDLE_PRIORITY + 4UL )
 
 // Stack sizes of our threads in words (4 bytes)
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define WORKER_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-#include "pico/async_context_freertos.h"
-static async_context_freertos_t async_context_instance;
+MPU6050 mpu;
+int16_t ax, ay, az; // Accelerometer data
+int16_t gx, gy, gz; // Gyroscope data
 
-// Create an async context
-static async_context_t *example_async_context(void) {
-    async_context_freertos_config_t config = async_context_freertos_default_config();
-    config.task_priority = WORKER_TASK_PRIORITY; // defaults to ASYNC_CONTEXT_DEFAULT_FREERTOS_TASK_PRIORITY
-    config.task_stack_size = WORKER_TASK_STACK_SIZE; // defaults to ASYNC_CONTEXT_DEFAULT_FREERTOS_TASK_STACK_SIZE
-    if (!async_context_freertos_init(&async_context_instance, &config))
-        return NULL;
-    return &async_context_instance.core;
-}
+void robot_task(__unused void *params) {
+    printf("Robot Task is starting!!!\n");
+    i2c_init(i2c_default, 400*1000);
 
-#if USE_LED
-// Turn led on or off
-static void pico_set_led(bool led_on) {
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-}
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    printf("MPU6050 initializing...\n");
+    mpu.initialize();
+    printf("MPU6050 initialized\n");
+    if(mpu.testConnection()) printf("MPU6050 connection successful\n");
+    else {printf("MPU6050 connection failed\n");}
 
-// Initialise led
-static void pico_init_led(void) {
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-}
+    // int16_t acceleration[3], gyro[3], temp;
 
-void blink_task(__unused void *params) {
-    bool on = false;
-    printf("blink_task starts\n");
-    pico_init_led();
-    while (true) {
-#if configNUMBER_OF_CORES > 1
-        static uint8_t last_core_id = -1;
-        if (portGET_CORE_ID() != last_core_id) {
-            last_core_id = portGET_CORE_ID();
-            printf("blink task is on core %d\n", last_core_id);
-        }
-#endif
-        printf("Blink task - Set Led: %d\n", on);
-        pico_set_led(on);
-        on = !on;
-
-#if LED_BUSY_WAIT
-        // You shouldn't usually do this. We're just keeping the thread busy,
-        // experiment with BLINK_TASK_PRIORITY and LED_BUSY_WAIT to see what happens
-        // if BLINK_TASK_PRIORITY is higher than TEST_TASK_PRIORITY main_task won't get any free time to run
-        // unless configNUMBER_OF_CORES > 1
-        busy_wait_ms(LED_DELAY_MS);
-#else
-        sleep_ms(LED_DELAY_MS);
-#endif
-    }
-}
-#endif // USE_LED
-
-void juan_task(__unused void *params) {
-    printf("Juan Task is starting!!!\n");
     while (true) {
         printf("Reading sensor data!\n");
-        vTaskDelay(300);
+        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+        printf("Acc. X = %d, Y = %d, Z = %d\n", ax, ay, az);
+        printf("Gyro. X = %d, Y = %d, Z = %d\n", gx, gy, gz);
+        vTaskDelay(500);
     }
 }
 
-// async workers run in their own thread when using async_context_freertos_t with priority WORKER_TASK_PRIORITY
-static void do_work(async_context_t *context, async_at_time_worker_t *worker) {
-    async_context_add_at_time_worker_in_ms(context, worker, 10000);
-    static uint32_t count = 0;
-    printf("Hello from worker count=%u\n", count++);
-#if configNUMBER_OF_CORES > 1
-        static uint8_t last_core_id = -1;
-        if (portGET_CORE_ID() != last_core_id) {
-            last_core_id = portGET_CORE_ID();
-            printf("worker is on core %d\n", last_core_id);
-        }
-#endif
-}
-async_at_time_worker_t worker_timeout = { .do_work = do_work };
 
 void main_task(__unused void *params) {
-    async_context_t *context = example_async_context();
-    // start the worker running
-    async_context_add_at_time_worker_in_ms(context, &worker_timeout, 0);
-#if USE_LED
     // start the led blinking
-    xTaskCreate(blink_task, "BlinkThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    xTaskCreate(juan_task, "JuanThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-#endif
-    int count = 0;
+    // xTaskCreate(blink_task, "BlinkThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+    xTaskCreate(robot_task, "JuanThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+    // int count = 0;
     while(true) {
-#if configNUMBER_OF_CORES > 1
-        static uint8_t last_core_id = -1;
-        if (portGET_CORE_ID() != last_core_id) {
-            last_core_id = portGET_CORE_ID();
-            printf("main task is on core %d\n", last_core_id);
-        }
-#endif
-        printf("Hello from main task count=%u\n", count++);
-        vTaskDelay(1000);
+        // printf("Hello from main task count=%u\n", count++);
+        // vTaskDelay(1000);
     }
-    async_context_deinit(context);
 }
 
 void vLaunch( void) {
@@ -161,23 +82,8 @@ int main( void )
 
     /* Configure the hardware ready to run the demo. */
     const char *rtos_name;
-#if (configNUMBER_OF_CORES > 1)
     rtos_name = "FreeRTOS SMP";
-#else
-    rtos_name = "FreeRTOS";
-#endif
-
-    mpu6050_init();
-#if (configNUMBER_OF_CORES > 1)
     printf("Starting %s on both cores:\n", rtos_name);
     vLaunch();
-#elif (RUN_FREE_RTOS_ON_CORE == 1 && configNUMBER_OF_CORES==1)
-    printf("Starting %s on core 1:\n", rtos_name);
-    multicore_launch_core1(vLaunch);
-    while (true);
-#else
-    printf("Starting %s on core 0:\n", rtos_name);
-    vLaunch();
-#endif
     return 0;
 }
